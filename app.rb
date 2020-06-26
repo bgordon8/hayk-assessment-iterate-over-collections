ce8e51adc574b4eaa5e6f469fa55a7ed087e2a23
bgordon8 <gordonbrian16@gmail.com>
2020-06-25 19:56:14 -0600

    this is my first commit

--- app.rb
+++ app.rb
@@ -1,3 +1,6 @@
+require 'pry'
+require 'rspec'
+
 class Company
     attr_accessor :name, :size
 
@@ -5,12 +8,37 @@ class Company
         @name = name
         @size = size
     end
+
 end
 
 companies = [
     Company.new('Alpha', 30),
-    Company.new('Beta, 300),
-    Company.new('Delta, 3000)
+    Company.new('Beta', 300),
+    Company.new('Delta', 3000)
 ]
 
-### Your code below this
+
+def list_of_name_and_size companies
+    companies.map { |company| company.name + " - " + company.size.to_s }
+end
+
+def all_sizes companies
+    companies.reduce(0) { |sum, company| sum + company.size}
+end
+
+def all_companies_over_100 companies
+    companies.select { |company| company.size > 100 }
+end
+
+def find_company_by_name companies
+    companies.find { |company| company.name == "Beta" }
+end
+
+def find_largest_company companies
+    companies.max_by(&:size)
+end
+
+def sort_by_largest companies
+    companies.sort_by(&:size).reverse!
+end
+binding.pry
